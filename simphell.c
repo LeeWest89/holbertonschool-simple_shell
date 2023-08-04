8e9b4b7f0c6d3c9b035023c35025ce175559a27c
LeeWest89 <6683@holbertonstudents.com>
2023-08-04 06:33:45 -0700

    trying for non-interactive

--- simphell.c
+++ simphell.c
@@ -9,15 +9,18 @@
 
 int main(int argc, char **argv)
 {
-	/*char *prompt = "simphell: ";*/
+	char *prompt = "simphell: ";
 	char *input = NULL, **token_argv;
 	const char *delim = " \n";
+	int interactive_mode = isatty(STDIN_FILENO);
 	(void)argc;
 	(void)argv;
 
 	while (1)
 	{
-		/*printf("%s", prompt);*/
+		if (interactive_mode)
+			printf("%s", prompt);
+
 		token_argv = tokenize(input, delim);
 		if (token_argv[0] != NULL && strcmp(token_argv[0], "exit") == 0)
 		{
